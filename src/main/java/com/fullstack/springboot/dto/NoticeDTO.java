21e9631a879b752de849a4b44be68fa30e07e1a0
Jihoon135224 <wl454519@gmail.com>
2024-12-30 11:19:36 +0900

    Add files via upload

---
+++ src/main/java/com/fullstack/springboot/dto/NoticeDTO.java
@@ -0,0 +1,25 @@
+package com.fullstack.springboot.dto.admin;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class NoticeDTO {
+	
+	private Long nno;
+	
+	private String title;
+	
+	private String content;
+	
+	private String imgPath;
+	
+	private boolean isPublic;
+	
+	private Long adno;
+}
